b20283b94f380da5396e3c47503211914b277101
Jan-Schatz <info@js-webdevelopment.de>
2023-05-03 08:38:33 +0200

    Add matrixplot and new param `additional_data`

--- models/chart.py
+++ models/chart.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional, Any
 from .config import Config
 
+
 class Chart(BaseModel):
     project: str
     step: str
@@ -9,4 +10,5 @@ class Chart(BaseModel):
     description: Optional[str] = None
     config: Config
     data: Optional[Any] = None
+    additional_data: Optional[Any] = None
     group_size: Optional[int] = None
