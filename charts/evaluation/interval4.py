bc23fa29636a8af4e880661ab0b03e5165d5760f
Jan-Schatz <info@js-webdevelopment.de>
2023-05-24 17:08:56 +0200

    First working version of interval4

--- charts/evaluation/interval4.py
+++ charts/evaluation/interval4.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,28 +17,62 @@ class Interval4(BaseChart):
 
         if ad is not None and not ad.empty:
             # Group the data by a categorical variable
-            grouped_data = df.groupby('Kategorie')
+            grouped_data = df.groupby(ad.iloc[:, 0])
 
-            # Loop over the groups and generate plots
-            for (group, group_df) in grouped_data:
-                # Calculate means and standard deviations of the data for each group
-                mean = group_df.mean()
-                stddev = group_df.std()
-                confidence_interval = 1.96 * stddev / np.sqrt(len(group_df))
+            # Define the order of categories
+            unique_values = ad.iloc[:, 0].unique()
 
-                # Plot the data for each group with error bars
-                plt.errorbar(x=np.arange(len(mean)), y=mean, yerr=confidence_interval,
-                             fmt='o', capsize=15, label=group)
+            # Determine the layout of subplots
+            n_cols = 2
+            n_subplots = len(df.columns)
 
-            # Add labels, title, and legend to the plot
-            plt.ylabel('Values')
-            # Hide x-axis labels
-            plt.xticks(np.arange(len(mean)), mean.index, rotation=45)
-            plt.title(title, fontsize=TITLE_FONT_SIZE, pad=TITLE_PADDING)
-            plt.legend(loc='best')
+            # Calculate the number of rows
+            n_rows = math.ceil(n_subplots / n_cols)
 
-            # Enable grid lines
-            plt.grid(b=True, which='both')
+            # Create subplots for each column
+            fig, axes = plt.subplots(
+                n_rows, n_cols, figsize=(15, n_rows * 5))
+
+            # Reshape the axes array
+            axes = axes.reshape(-1)
+
+            # Loop over the columns and generate plots
+            for i, column in enumerate(df.columns):
+                ax = axes[i]
+
+                # Loop over the unique values and categories simultaneously
+                for j, (value, category) in enumerate(zip(unique_values, ad.iloc[:, 0])):
+                    # Get the data for the current category and column
+                    group_df = grouped_data.get_group(category)
+                    values = group_df[column]
+
+                    # Calculate mean and confidence interval
+                    mean = np.mean(values)
+                    stddev = np.std(values)
+                    confidence_interval = 1.96 * stddev / np.sqrt(len(values))
+
+                    # Plot the data for each category with error bars
+                    ax.errorbar(x=j, y=mean, yerr=confidence_interval,
+                                fmt='o', capsize=15, label=category)
+
+                # Set the x-axis tick positions and labels
+                ax.set_xticks(range(len(unique_values)))
+                ax.set_xticklabels(unique_values)
+
+                # Add labels, title, and legend to the plot
+                ax.set_ylabel('Values')
+                ax.set_title(column)
+                ax.legend(loc='best')
+
+                # Enable grid lines
+                ax.grid(b=True, which='both')
+
+            # Hide any extra subplots
+            for i in range(n_subplots, len(axes)):
+                fig.delaxes(axes[i])
+
+            # Adjust the subplot layout
+            plt.subplots_adjust(hspace=0.4)
 
         else:
             # Loop over pd an genrate plots
@@ -61,5 +96,8 @@ class Interval4(BaseChart):
 
                 # Enable grid lines
                 plt.grid(b=True, which='both')
+
+        # Adjust the subplot layout
+        plt.tight_layout()
 
         return plt
