7440b17d23467e7c6cbab591461612a9d2d3d9db
Andre <andre.daberto@hotmail.com>
2024-08-21 11:41:10 +0200

    MSA 1,2,3 and Attribute gage finished

--- models/config.py
+++ models/config.py
@@ -6,6 +6,9 @@ class Config(BaseModel):
     labelx: Optional[str] = None
     labely: Optional[str] = None
     labels: Optional[list] = None
+    trials: Optional[int] = None
     reference: Optional[float] = None
     tolerance: Optional[float] = None
     percentage_of_tolerance: Optional[float] = None
+    lcl: Optional[float] = None
+    ucl: Optional[float] = None
