b43aeb3883dabca847fe6cf711660242dbc7ca1b
philipp.aichner@fullet.at <philipp.aichner@fullet.at>
2025-02-26 08:50:49 +0100

    added routes for paired-T-test and F-test

--- api/charts/hypothesistest/hypothesistest_router.py
+++ api/charts/hypothesistest/hypothesistest_router.py
@@ -16,3 +16,15 @@ async def generate_ttest(request: dict):
 async def generate_twottest(request: dict):
     from .twottest import TwoTtest
     return await generate_chart(request, TwoTtest, "error_processing", extension="pdf")
+
+@router.post("/pairedttest")
+async def generate_pairedttest(request: dict):
+    from .paired_ttest import PairedTtest
+    return await generate_chart(request, PairedTtest, "error_processing", extension="pdf")
+
+# Anova
+
+@router.post("/ftest")
+async def generate_ftest(request: dict):
+    from .ftest import Ftest
+    return await generate_chart(request, Ftest, "error_processing", extension="pdf")
